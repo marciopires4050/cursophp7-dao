8d26c07dd31c5865530d8e492e2249aa35d77e6a
marciopires <mpbox80@gmail.com>
2019-04-16 19:03:14 -0300

    Métodos de listas

--- index.php
+++ index.php
@@ -8,10 +8,25 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
 
-$root->loadbyId(1);
+// Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(1);
+//echo $root;
 
-echo $root;
+// Carrega uma lista usuário
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Carrega uma lista de usuários buscando pelo login
+//$seach = Usuario::search("jo");
+//echo json_encode($seach);
+
+// Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+
+echo $usuario;
+
 
 ?>
